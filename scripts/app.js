a263aac97f56cb2f7868c679791f2f4663cebf23
amandalee anderson <amandalee.anderson@gmail.com>
2016-10-24 11:34:10 -0500

    this makes tons more sense

--- scripts/app.js
+++ scripts/app.js
@@ -13,35 +13,39 @@ $(document).ready(function(){
   var availableSquares = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   var xPlays = [];
   var oPlays = [];
+  var turn = 0;
 
+//assuming the user goes first
+  $(".square").click(function(){
+    //IT IS THE USER'S TURN
+    $(this).text("X");
+    var classlist = $(this).attr("class").split(' ');
+    var classnumber = classlist[classlist.length-1];
+    var num = parseInt(classnumber.split("-").pop());//refactor these last two lines to be one.
+    
 
-  var turn = 0; 
-  for (var i = 0; i++; i > 9) { //maximum of n
-    //if i is odd, it's the user's turn
-      $(".square").click(function(){
-        $(this).text("X");
-        //get the number off the square class here and push is to the x array
-        //and remove it from the availableSquares array
-
-      });
-    //if i is even, it's the browser's turn
-      //generate a umber between 1 and 9
-
-      //append that to square and get element the class of square* on it. 
-
-      //check whether this has a number in it already; if it does, repeat this function.
-
-      //after it finds a spot that isn't 
-    //if i >= 5
-      // after every turn, check to see whether the
-  }
-
-  //user's turn 
-  
-  //computer's turn
-  $(".your-turn").click(function(){
-    //generate a n 
+    //get the number off the square class here and push is to the x array
+    //and remove it from the availableSquares array
+    //and push it to the xplays array
+    //turn ++; 
+    //if turn > 5
+      //check to see whether xplays array is equal to any of the arrays in wins 
+      //if so, game over X wins. 
+    //NOW IT IS THE BROWSER'S TURN
+      //delay for a second or two
+      //generate a random number betwee 1 and 9
+      //append that to square and get element with class of square-* on it
+      //if it's empty, put an O in that element. 
+      //and remover it from the availableSquares array
+      //and push it to the oplays array
+      //turn++
+      // if turn > 6
+        //check to see whether oplays array is equal to any of the arrays in wins 
+        //if so, game over, O wins. 
 
   });
+
+  
+
 });
 
