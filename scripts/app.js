c929dc4698085b1f75f7e9ebb1fe801de749cc13
amandalee anderson <amandalee.anderson@gmail.com>
2016-10-24 16:47:39 -0500

    updates

--- scripts/app.js
+++ scripts/app.js
@@ -11,8 +11,7 @@ $(document).ready(function(){
     [3, 5, 7]
   ];
 
-  //creates a method on Array object so we can find an array within an array - this is pure StackOverflow work, but it made the most sense. 
-
+  //creates a method on Array object so we can find an array within an array 
   Array.prototype.containsArray = function(val) {
     var hash = {};
     for(var i=0; i<this.length; i++) {
@@ -21,34 +20,73 @@ $(document).ready(function(){
     return hash.hasOwnProperty(val);
   } 
 
+  // function to generate a random number for the browser's turn. 
+  function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
   var availableSquares = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   var xPlays = [];
   var oPlays = [];
   var turn = 0;
 
-//assuming the user goes first
+  //setting up the browser's turn as its own function
+  var browsersTurn = function(){
+    var browsersSquareNumber = getRandomIntInclusive(1, availableSquares.length); 
+    console.log(browsersSquareNumber + " is the number the browser is picking w random");
+    var browserChoice = availableSquares[browsersSquareNumber];
+    console.log(availableSquares[browsersSquareNumber] + " is the box the browser is picking from the availableSquares array");
+    var $browserSquare = $(".square-" + browserChoice);
+    // console.log($browserSquare + " is the browsersquare");
+    $browserSquare.text("O");
+    $(".turn-display").text("It's your turn!");
+
+    //find it within the availableSquares array, and get rid of it
+    var index = availableSquares.indexOf(browserChoice);
+    availableSquares.splice(index, 1);
+    console.log(availableSquares);
+
+    //and push it to the oplays array
+    oPlays.push(browserChoice);
+    oPlays.sort(function(a, b){return a-b});
+    console.log(oPlays);
+    
+    if (turn >=3 ) {
+      var indexOfO = wins.containsArray(oPlays); //this is neato
+      console.log(indexOfO);
+      if (indexOfO === true) {
+        alert("you win");
+      }
+      // TO FIX: if O has had more than 3 turns before they win, this doesn't work. 
+    }
+  }
+
+
+  //the user goes first
+
   $(".square").click(function(){
     //IT IS THE USER'S TURN
     //check whether this square already has something in it. 
     if ($.trim($(this).html())=='') {
       $(this).text("X");
+      $(".turn-display").text("It's the browser's turn!");
     } else {
-      alert("nope");
+      alert("nope");//fix and do something funny here
     }
-    
+    //get the classes on the square the user clicked
     var classlist = $(this).attr("class").split(' ');
-    var classnumber = classlist[classlist.length-1];
-    var num = parseInt(classnumber.split("-").pop());//refactor these last two lines to be one.
-  
-    //find it within the availablesquares array, and get rid of it
-    var index = availableSquares.indexOf(num);
+    //get the number at the end of the last one.
+    var userChoice = parseInt((classlist[classlist.length-1]).split("-").pop());
+
+    //find it within the availableSquares array, and get rid of it
+    var index = availableSquares.indexOf(userChoice);
     availableSquares.splice(index, 1);
 
     //and push it to the xplays array
-    xPlays.push(num);
+    xPlays.push(userChoice);
     xPlays.sort(function(a, b){return a-b}); //sort it - not sure if we need this.
-    console.log(xPlays); //
-    console.log(wins);
 
     //increment the turn
     turn ++; 
@@ -60,28 +98,16 @@ $(document).ready(function(){
       var indexOfX = wins.containsArray(xPlays); //this is neato
       console.log(indexOfX);
       if (indexOfX === true) {
-        alert("you win");
-        //x wins, are we keeping track of how many wins? 
-        //localstorage 
+        alert("the browser wins");
+        //x wins, are we keeping track of how many wins? possibly in localstorage? 
       }
       // TO FIX: if X has had more than 3 turns before they win, this doesn't work. 
     }
       
     //NOW IT IS THE BROWSER'S TURN
-      //delay for a second or two
-      //generate a random number betwee 1 and 9
-      //append that to square and get element with class of square-* on it
-      //if it's empty, put an O in that element. 
-      //and remover it from the availableSquares array
-      //and push it to the oplays array
-      //turn++
-      // if turn > 6
-        //check to see whether oplays array is equal to any of the arrays in wins 
-        //if so, game over, O wins. 
+    setTimeout(browsersTurn, 1000)
 
   });
-
-  
 
 });
 
